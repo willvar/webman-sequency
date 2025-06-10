ae6349ff6d165c9a8dc10ebacd4396d79d41c78e
William Varmus <0@willvar.tw>
2025-06-10 21:24:03 +0800

    FIX: Install.php 未创建消费进程导致无法任务被消费

--- src/Install.php
+++ src/Install.php
@@ -17,6 +17,30 @@ class Install
     if (!is_dir($consumer_dir)) {
       mkdir($consumer_dir, 0777, true);
     }
+    // 注册消费者进程
+    $config_dir = config_path() . '/plugin/webman/sequency';
+    if (!is_dir($config_dir)) {
+      mkdir($config_dir, 0777, true);
+    }
+    $process_config_file = $config_dir . '/process.php';
+    if (!is_file($process_config_file)) {
+      $content = <<<'EOF'
+<?php
+
+use Webman\Sequency\Process\Consumer;
+
+return [
+    'consumer' => [
+        'handler' => Consumer::class,
+        'constructor' => [
+            // 消费者类所在的目录
+            'consumer_dir' => app_path() . '/queue/sequency'
+        ]
+    ]
+];
+EOF;
+      file_put_contents($process_config_file, $content);
+    }
   }
 
   /**
@@ -25,5 +49,10 @@ class Install
    */
   public static function uninstall(): void
   {
+    // 移除插件的进程配置文件
+    $process_config_file = config_path() . '/plugin/webman/sequency/process.php';
+    if (is_file($process_config_file)) {
+      unlink($process_config_file);
+    }
   }
 }
